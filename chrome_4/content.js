5f6911bf7a7af5937b952a287b659ec6d6b0d3fa
Lisa Ballard <lballard.cat@gmail.com>
2016-06-22 17:08:17 +0100

    adds ajax call and word count, still just alerting to screen

--- chrome_4/content.js
+++ chrome_4/content.js
@@ -2,6 +2,7 @@
 
 function getselection() {
     var text = "";
+
     if (window.getSelection) {
         text = window.getSelection().toString();
     } else if (document.selection && document.selection.type != "Control") {
@@ -10,19 +11,24 @@ function getselection() {
     return text;
 }
 
-
-
-
-
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
+      // var firstHref = $("a[href^='http']").eq(0).attr("href");
+      var count = getselection().split(' ').length;
 
-      console.log(firstHref);
-      console.log(getselection());
+      var username = 'Homer'; // hard coding this for now
+      var url = 'https://obscure-cliffs-10478.herokuapp.com/' + username + '/add/';
+
+      word_count = { 'count': count }  // this gets build from user highlight/button click
+
+      var posting = $.post( url, word_count );
+
+      posting.done(function( html ) {
+        alert(html);
+      });
+
 
     }
   }
 );
-
