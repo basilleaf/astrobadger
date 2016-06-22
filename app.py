1192da546b10bda86e73a3465b603fe2acabc0ad
Lisa Ballard <lballard.cat@gmail.com>
2016-06-22 16:18:27 +0100

    adds index page and single user view response to post

--- app.py
+++ app.py
@@ -11,12 +11,20 @@ r_server = redis.StrictRedis.from_url(REDIS_URL)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
+@app.route('/')
+def index():
+    all_badgers = loads(r_server.get('all_badgers'))
+    all_badgers_sorted = collections.OrderedDict(sorted(all_badgers.items()))
+    kwargs = {'all_badgers': all_badgers_sorted}
+    return render_template('index.html', **kwargs)
+
+
 @app.route('/<username>/add/', methods=['POST'])
 def add(username):
     word_count = request.form['count']
     dt = datetime.now()
     post_date = dt.isoformat("T").split('T')[0]
-    
+
     all_badgers = loads(r_server.get('all_badgers'))
     all_badgers.setdefault(username, {})
     all_badgers[username][post_date] = word_count
@@ -24,10 +32,10 @@ def add(username):
     # save to db
     r_server.set('all_badgers', dumps(all_badgers))
 
-    #
-    all_badgers_sorted = collections.OrderedDict(sorted(all_badgers.items()))
-    kwargs = {'all_badgers': all_badgers_sorted}
-    return render_template('index.html', **kwargs)
+    this_badger = all_badgers[username]
+    this_badger_sorted = collections.OrderedDict(sorted(this_badger.items()))
+    kwargs = {'username': username, 'badger': this_badger}
+    return render_template('user.html', **kwargs)
 
 
 if __name__ == '__main__':
