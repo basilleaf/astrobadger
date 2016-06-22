f77209c717ea5fe7ba1b6017f6d98d13bfdcc2d1
Lisa Ballard <lballard.cat@gmail.com>
2016-06-22 18:17:47 +0100

    adds overlay display bubble

--- chrome_4/content.js
+++ chrome_4/content.js
@@ -11,21 +11,46 @@ function getselection() {
     return text;
 }
 
+
+// Move that bubble to the appropriate location.
+// thanks http://stackoverflow.com/questions/4409378/text-selection-and-bubble-overlay-as-chrome-extension
+function renderBubble(html) {
+    // Add bubble to the top of the page.
+    var bubbleDOM = document.createElement('div');
+    bubbleDOM.setAttribute('class', 'selection_bubble');
+    document.body.appendChild(bubbleDOM);
+
+    // Close the bubble when we click on the screen.
+    // todo: replace this so it doesn't keep adding same even listener
+    document.addEventListener('mousedown', function (e) {
+      bubbleDOM.style.visibility = 'hidden';
+    }, false);
+
+    bubbleDOM.innerHTML = html;
+
+    bubbleDOM.style.visibility = 'visible';
+
+}
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
       // var firstHref = $("a[href^='http']").eq(0).attr("href");
       var count = getselection().split(' ').length;
 
-      var username = 'Homer'; // hard coding this for now
-      var url = 'https://obscure-cliffs-10478.herokuapp.com/' + username + '/add/';
+      var username = 'Matt'; // hard coding this for now
+      var url = 'https://obscure-cliffs-10478.herokuapp.com/';
+      // var url = 'http://127.0.0.1:5000/';  // dev
 
       word_count = { 'count': count }  // this gets build from user highlight/button click
 
-      var posting = $.post( url, word_count );
+      var posting = $.post( url + username + '/add/', word_count );
 
       posting.done(function( html ) {
-        alert(html);
+
+        renderBubble(html);
+
+
       });
 
 
